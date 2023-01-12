33a230cf7e27b7732a17e7a242652310535f1de6
Szymon <s205859@sggw.edu.pl>
2023-01-12 19:54:41 +0100

    Hybrid model is finally working

--- main.py
+++ main.py
@@ -1,7 +1,96 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from methods import *
 from sklearn.ensemble import VotingClassifier
+
+
+def make_hybrid_x(methods):
+    hybrid_train = np.array([methods[0]])
+    for i in range(1, methods.__len__()):
+        hybrid_train = np.r_[hybrid_train, [methods[i]]]
+
+    hybrid_train = hybrid_train.transpose()
+    return hybrid_train
+
+
+def make_hybrid_model(x_train, x_test, y_train, y_test):
+    wojtek_train = x_train[:, [33, 62, 10, 21, 49, 15, 18, 6, 48, 51, 24, 12, 19]]
+    wojtek_test = x_test[:, [33, 62, 10, 21, 49, 15, 18, 6, 48, 51, 24, 12, 19]]
+    # wojtek_val = x_val[:, [33, 62, 10, 21, 49, 15, 18, 6, 48, 51, 24, 12, 19]]
+
+    jakub_train = x_train[:, [7, 8, 22, 26, 29, 34, 35, 37, 40, 45, 47, 52, 61]]
+    jakub_test = x_test[:, [7, 8, 22, 26, 29, 34, 35, 37, 40, 45, 47, 52, 61]]
+    # jakub_val = x_val[:, [7, 8, 22, 26, 29, 34, 35, 37, 40, 45, 47, 52, 61]]
+
+    szymon_train = x_train[:, [1, 9, 16, 20, 36, 38, 41, 42, 43, 44, 53, 58, 59]]
+    szymon_test = x_test[:, [1, 9, 16, 20, 36, 38, 41, 42, 43, 44, 53, 58, 59]]
+    # szymon_val = x_val[:, [1, 9, 16, 20, 36, 38, 41, 42, 43, 44, 53, 58, 59]]
+
+    maciek_train = x_train[:, [9, 50, 54, 5, 32, 27, 28, 30, 25, 11, 3, 60, 0]]
+    maciek_test = x_test[:, [9, 50, 54, 5, 32, 27, 28, 30, 25, 11, 3, 60, 0]]
+    # maciek_val = x_val[:, [9, 50, 54, 5, 32, 27, 28, 30, 25, 11, 3, 60, 0]]
+
+    bartek_train = x_train[:, [23, 55, 57, 17, 56, 13, 39, 46, 31, 14, 63, 64, 2]]
+    bartek_test = x_test[:, [23, 55, 57, 17, 56, 13, 39, 46, 31, 14, 63, 64, 2]]
+    # bartek_val = x_val[:, [23, 55, 57, 17, 56, 13, 39, 46, 31, 14, 63, 64, 2]]
+
+    # wojtek: decision tree, random forest, naive bayes, logistic regression
+    # jakub: knn, random forest, svc, naive bayes
+    # szymon: linear_discriminant, natural_network, gradient_boosting, radius_neighbors
+    # maciek: logistic regression, sgd, natural_network, gradient_boosting
+    # bartek: knn, svc, decision tree, svr
+
+    wojtek_dt = decision_tree(wojtek_train, y_train, wojtek_test)
+    wojtek_rf = random_forest(wojtek_train, y_train, wojtek_test)
+    wojtek_gnb = gaussian_naive_bayes(wojtek_train, y_train, wojtek_test)
+    wojtek_lg = logistic_regression(wojtek_train, y_train, wojtek_test)
+
+    jakub_rf = random_forest(jakub_train, y_train, jakub_test)
+    jakub_gnb = gaussian_naive_bayes(jakub_train, y_train, jakub_test)
+    jakub_knn = KNN(jakub_train, y_train, jakub_test)
+    jakub_svc = SVC(jakub_train, y_train, jakub_test)
+
+    bartek_dt = decision_tree(bartek_train, y_train, bartek_test)
+    bartek_knn = KNN(bartek_train, y_train, bartek_test)
+    bartek_svc = SVC(bartek_train, y_train, bartek_test)
+    # bartek_svr = svr(bartek_train, y_train, bartek_test)
+
+    maciek_lg = logistic_regression(maciek_train, y_train, maciek_test)
+    maciek_nn = natural_network(maciek_train, y_train, maciek_test)
+    maciek_gb = gradient_boosting(maciek_train, y_train, maciek_test)
+    maciek_sgd = SGD(maciek_train, y_train, maciek_test)
+
+    szymon_ld = linear_discriminant_analysis(szymon_train, y_train, szymon_test)
+    szymon_nn = natural_network(szymon_train, y_train, szymon_test)
+    szymon_gb = gradient_boosting(szymon_train, y_train, szymon_test)
+    szymon_rn = radius_neighbors(szymon_train, y_train, szymon_test)
+
+    models =  [
+        wojtek_dt,
+        wojtek_rf,
+        wojtek_gnb,
+        wojtek_lg,
+        jakub_rf,
+        jakub_gnb,
+        jakub_knn,
+        jakub_svc,
+        bartek_dt,
+        bartek_knn,
+        bartek_svc,
+        # bartek_svr,
+        maciek_lg,
+        maciek_nn,
+        maciek_gb,
+        maciek_sgd,
+        szymon_ld,
+        szymon_nn,
+        szymon_gb,
+        szymon_rn,
+    ]
+    for model in  models:
+        print(score(model,y_test))
+    return models
 
 df = pd.read_csv('./Arkusz2.csv', sep=';')
 
@@ -14,79 +103,20 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25, random_state=8)
 
+methods = make_hybrid_model(x_train, x_train, y_train, y_train)
+hybrid_train = make_hybrid_x(methods)
+hybrid_model = natural_network(hybrid_train, y_train, hybrid_train)
 
-wojtek_train = x_train[:, [33, 62, 10, 21, 49, 15, 18, 6, 48, 51, 24, 12, 19]]
-wojtek_test = x_test[:, [33, 62, 10, 21, 49, 15, 18, 6, 48, 51, 24, 12, 19]]
-wojtek_val = x_val[:, [33, 62, 10, 21, 49, 15, 18, 6, 48, 51, 24, 12, 19]]
+print(hybrid_model)
+print(score(hybrid_model, y_train))
+print('!!!!')
+methods2 = make_hybrid_model(x_train, x_test, y_train, y_test)
+hybrid_test = make_hybrid_x(methods2)
+hybrid_model2 = natural_network(hybrid_train, y_train, hybrid_test)
 
-jakub_train = x_train[:, [7, 8, 22, 26, 29, 34, 35, 37, 40, 45, 47, 52, 61]]
-jakub_test = x_test[:, [7, 8, 22, 26, 29, 34, 35, 37, 40, 45, 47, 52, 61]]
-jakub_val = x_val[:, [7, 8, 22, 26, 29, 34, 35, 37, 40, 45, 47, 52, 61]]
+print(hybrid_model2)
+print(score(hybrid_model2, y_test))
+print('!!!!')
 
-szymon_train = x_train[:, [1, 9, 16, 20, 36, 38, 41, 42, 43, 44, 53, 58, 59]]
-szymon_test = x_test[:, [1, 9, 16, 20, 36, 38, 41, 42, 43, 44, 53, 58, 59]]
-szymon_val = x_val[:, [1, 9, 16, 20, 36, 38, 41, 42, 43, 44, 53, 58, 59]]
-
-maciek_train = x_train[:, [9, 50, 54, 5, 32, 27, 28, 30, 25, 11, 3, 60, 0]]
-maciek_test = x_test[:, [9, 50, 54, 5, 32, 27, 28, 30, 25, 11, 3, 60, 0]]
-maciek_val = x_val[:, [9, 50, 54, 5, 32, 27, 28, 30, 25, 11, 3, 60, 0]]
-
-bartek_train = x_train[:, [23, 55, 57, 17, 56, 13, 39, 46, 31, 14, 63, 64, 2]]
-bartek_test = x_test[:, [23, 55, 57, 17, 56, 13, 39, 46, 31, 14, 63, 64, 2]]
-bartek_val = x_val[:, [23, 55, 57, 17, 56, 13, 39, 46, 31, 14, 63, 64, 2]]
-
-#wojtek: decision tree, random forest, naive bayes, logistic regression
-#jakub: knn, random forest, svc, naive bayes
-#szymon: linear_discriminant, natural_network, gradient_boosting, radius_neighbors
-#maciek: logistic regression, sgd, natural_network, gradient_boosting
-#bartek: knn, svc, decision tree, svr
-
-wojtek_dt = decision_tree(wojtek_train, y_train)
-wojtek_rf = random_forest(wojtek_train, y_train)
-wojtek_gnb = gaussian_naive_bayes(wojtek_train, y_train)
-wojtek_lg = logistic_regression(wojtek_train, y_train)
-
-jakub_rf = random_forest(jakub_train, y_train)
-jakub_gnb = gaussian_naive_bayes(jakub_train, y_train)
-jakub_knn = KNN(jakub_train, y_train)
-jakub_svc = SVC(jakub_train, y_train)
-
-bartek_dt = decision_tree(bartek_train, y_train)
-bartek_knn = KNN(bartek_train, y_train)
-bartek_svc = SVC(bartek_train, y_train)
-bartek_svr = svr(bartek_train, y_train)
-
-maciek_lg = logistic_regression(maciek_train, y_train)
-maciek_nn = natural_network(maciek_train, y_train)
-maciek_gb = gradient_boosting(maciek_train, y_train)
-maciek_sgd = SGD(maciek_train, y_train)
-
-szymon_ld = linear_discriminant_analysis(szymon_train, y_train)
-szymon_nn = natural_network(szymon_train, y_train)
-szymon_gb = gradient_boosting(szymon_train, y_train)
-szymon_rn = radius_neighbors(szymon_train, y_train)
-
-eclf1 = VotingClassifier(estimators=[
-    ('wdt', wojtek_dt),
-    ('wrf', wojtek_rf),
-    ('wgnb', wojtek_gnb),
-    ('wlg', wojtek_lg),
-    ('jrf', jakub_rf),
-    ('jgnb', jakub_gnb),
-    ('jknn', jakub_knn),
-    ('jsvr', jakub_svc),
-    ('bdt', bartek_dt),
-    ('bknn', bartek_knn),
-    ('bsvc', bartek_svc),
-    ('bsvr', bartek_svr),
-    ('mlg', maciek_lg),
-    ('mnn', maciek_nn),
-    ('mgb', maciek_gb),
-    ('msgd', maciek_sgd),
-    ('sld', szymon_ld),
-    ('snn', szymon_nn),
-    ('sgb', szymon_gb),
-    ('sr', szymon_rn),
-], voting='soft')
 # eclf1 = eclf1.fit(x, y)
 # print(eclf1.predict(x))
--- methods.py
+++ methods.py
@@ -10,81 +10,95 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import SGDClassifier
 
 
-def decision_tree(x_train, y_train):
+def decision_tree(x_train, y_train, x_test):
     dt = DecisionTreeClassifier(random_state=0)
     dt.fit(x_train, y_train.ravel())
 
-    return dt
+    return dt.predict(x_test)
 
 
-def random_forest(x_train, y_train):
+def random_forest(x_train, y_train, x_test):
     rf = RandomForestClassifier(max_depth=15, random_state=0)
     rf.fit(x_train, y_train.ravel())
 
-    return rf
+    return rf.predict(x_test)
 
 
-def gaussian_naive_bayes(x_train, y_train):
+def gaussian_naive_bayes(x_train, y_train, x_test):
     gnb = GaussianNB()
     gnb.fit(x_train, y_train.ravel())
 
-    return gnb
+    return gnb.predict(x_test)
 
 
-def logistic_regression(x_train, y_train):
+def logistic_regression(x_train, y_train, x_test):
     lg = LogisticRegression(random_state=0)
     lg.fit(x_train, y_train.ravel())
 
-    return lg
+    return lg.predict(x_test)
 
-def KNN(x_train, y_train):
+
+def KNN(x_train, y_train, x_test):
     knn = KNeighborsClassifier(n_neighbors=2)  # n_neighbors=10
     knn.fit(x_train, y_train.ravel())
-    return knn
+    return knn.predict(x_test)
 
-def SVC(x_train, y_train):
+
+def SVC(x_train, y_train, x_test):
     from sklearn.svm import SVC
     svc = SVC(probability=True, kernel='linear')
     svc.fit(x_train, y_train.ravel())
 
-    return svc
+    return svc.predict(x_test)
 
 
-def linear_discriminant_analysis(x_train, y_train):
+def linear_discriminant_analysis(x_train, y_train, x_test):
     clf = LinearDiscriminantAnalysis()
     clf.fit(x_train, y_train.ravel())
 
-    return clf
+    return clf.predict(x_test)
 
 
-def natural_network(x_train, y_train):
-    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(9, 5, 3), random_state=1, max_iter=6000)
+def natural_network(x_train, y_train, x_test):
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(14, 8, 6, 3, 2), random_state=1, max_iter=6000)
     clf.fit(x_train, y_train.ravel())
 
-    return clf
+    return clf.predict(x_test)
 
-def gradient_boosting(x_train, y_train):
+
+def gradient_boosting(x_train, y_train, x_test):
     clf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0)
     clf.fit(x_train, y_train.ravel())
 
-    return clf
+    return clf.predict(x_test)
 
-def radius_neighbors(x_train, y_train):
+
+def radius_neighbors(x_train, y_train, x_test):
     clf = RadiusNeighborsClassifier(radius=1.8)
     clf.fit(x_train, y_train.ravel())
 
-    return clf
+    return clf.predict(x_test)
 
-def svr(x_train, y_train):
+
+def svr(x_train, y_train, x_test):
     from sklearn.svm import SVR
 
     svr = SVR(C=1.0, epsilon=0.2)
-    svr.fit(x_train, y_train)
+    svr.fit(x_train, y_train.ravel())
 
-    return svr
+    return svr.predict(x_test)
 
-def SGD(x_train, y_train):
+
+def SGD(x_train, y_train, x_test):
 	sgd_mck = SGDClassifier()
 	sgd_mck.fit(x_train, y_train.ravel())
 
-	return sgd_mck
+	return sgd_mck.predict(x_test)
+
+
+def score(y_predict, y):
+    x = 0.0
+    for i in range(0, y.__len__()):
+        if y_predict[i] == y[i]:
+            x = x + 1
+    return x/y.__len__()
